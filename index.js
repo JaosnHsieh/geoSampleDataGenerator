8c14097cc87ae637116253355eb3182af1b2fa0c
jasonhsieh <jasonhsieh@ghtinc.com>
2018-06-08 09:34:17 +0800

    polygon fixed

--- index.js
+++ index.js
@@ -7,13 +7,20 @@ const data2csv = rows => rows.map(row => row.join(",")).join("\n");
 $("#downloadBtn").on("click", () => {
   const numbers = parseInt($("#polygonNumbers")[0].value);
   const randomRawData = random.polygon(numbers).features;
+  console.log(JSON.stringify(randomRawData));
   const formatDataForCsv = [["name", "lat", "lon"]].concat(
     randomRawData.map(d => {
       const row = [`name${(Math.random() * 1000000).toFixed(0)}`];
       const coordinates = d.geometry.coordinates[0];
       coordinates.forEach(c => {
-        row.push(c[1]);
-        row.push(c[0]);
+        let lat = c[0];
+        let lon = c[1];
+        if (lat > 90) lat = 89 + Math.random();
+        if (lat < -90) lat = -89 - Math.random();
+        if (lon > 180) lon = 179 + Math.random();
+        if (lon < -180) lon = -179 - Math.random();
+        row.push(lat);
+        row.push(lon);
       });
       return row;
     })
