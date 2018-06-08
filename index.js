8e0fccddd267b09d0487c87efa7380385ee666a6
jasonhsieh <jasonhsieh@ghtinc.com>
2018-06-08 11:58:53 +0800

    add circle generate and limit to taiwan

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var $ = require("jquery");
 
 const data2csv = rows => rows.map(row => row.join(",")).join("\n");
 
-$("#downloadBtn").on("click", () => {
+$("#downloadBtn1").on("click", () => {
   const numbers = parseInt($("#polygonNumbers")[0].value);
   const randomRawData = random.polygon(numbers).features;
   console.log(JSON.stringify(randomRawData));
@@ -25,5 +25,37 @@ $("#downloadBtn").on("click", () => {
       return row;
     })
   );
-  downloadjs(data2csv(formatDataForCsv), `sample_${numbers}.csv`);
+  downloadjs(data2csv(formatDataForCsv), `sample_${numbers}_polygon.csv`);
+});
+
+$("#downloadBtn2").on("click", () => {
+  const numbers = parseInt($("#circleNumbers")[0].value);
+  const randomRawData = random.point(numbers, [
+    21.970391,
+    119.879572,
+    25.327096,
+    122.571706
+  ]).features;
+  console.log(randomRawData);
+  // console.log(JSON.stringify(randomRawData));
+  const formatDataForCsv = [["name", "radius(m)", "lat", "lon"]].concat(
+    randomRawData.map(d => {
+      const row = [`name${(Math.random() * 1000000).toFixed(0)}`];
+      const randomRaidus = Math.floor(Math.random() * 10000 + 1000);
+      const coordinate = d.geometry.coordinates;
+      let lat = coordinate[0];
+      let lon = coordinate[1];
+      if (lat > 90) lat = 89 + Math.random();
+      if (lat < -90) lat = -89 - Math.random();
+      if (lon > 180) lon = 179 + Math.random();
+      if (lon < -180) lon = -179 - Math.random();
+
+      row.push(randomRaidus);
+      row.push(lat);
+      row.push(lon);
+      return row;
+    })
+  );
+  console.log(formatDataForCsv);
+  downloadjs(data2csv(formatDataForCsv), `sample_${numbers}_circle.csv`);
 });
